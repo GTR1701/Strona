d1cdb2edad811da23d0d7f6bd78c5fedb61ef015
GTR1701 <dawidkowal04@gmail.com>
2022-09-13 20:03:40 +0200

    Working Button

--- src/components/button/component.button.jsx
+++ src/components/button/component.button.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './component.button.css'
 
-function NavButton(content, ...props) {
+function NavButton({content: {id,content}, ...props}) {
     return (
         <a href="" className="nav-bar-button nav-bar-left">{content}</a>
     );
