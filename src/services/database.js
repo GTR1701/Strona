ec523f821a2aab4db1500189ca7ffae0a8a30cdc
GTR1701 <dawidkowal04@gmail.com>
2022-10-02 12:38:39 +0200

    Database problems

--- src/services/database.js
+++ src/services/database.js
@@ -1,17 +1,26 @@
-
 let mysql = require('mysql')
+let con = mysql.createConnection({
+        host: "127.0.0.1",
+        user: "root",
+        password: "",
+        database: "logowanie"
+});
 
-class database {
-    constructor(){
-        this.con = mysql.createConnection({
-            host: "127.0.0.1",
-            user: "root",
-            password: "",
-            database: "logowanie"
-          });
-        
-        
-    }
-    
+const insert = (query) => {
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+        // var sql = "INSERT INTO logowanie (imie, nazwisko) VALUES ('test1', 'test1')";
+        con.query(query, function (err, result) {
+            if (err) throw err;
+            console.log("1 record inserted");
+        });
+    });
 }
-export default database;
+
+class Database {
+    databaseInjection = (query) => {
+        insert(query)
+    } 
+}
+export default Database;
