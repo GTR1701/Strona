dd604b4e9f4861319c9053c25a66e1c1d7c6982c
GTR1701 <dawidkowal04@gmail.com>
2022-10-28 22:04:21 +0200

    Początek responsywności

--- Client/src/components/login-modal/component.loginModal.jsx
+++ Client/src/components/login-modal/component.loginModal.jsx
@@ -1,21 +1,18 @@
-import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import './component.loginModal.css'
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import "./component.loginModal.css";
 
 function LoginModal() {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-
   return (
     <>
-      <div className='login'>
-        <button className='login' onClick={handleShow}>
-          Zaloguj
-        </button>
-      </div>
+      <button className="login" onClick={handleShow}>
+        Zaloguj
+      </button>
 
       <Modal
         show={show}
@@ -28,15 +25,25 @@ function LoginModal() {
         </Modal.Header>
         <Modal.Body>
           <center>
-            <form id='login-form' className='login-form'>
-              <label htmlFor="Login">Login</label><br></br>
-              <input type="text" id='login' name='Login' /><br></br>
-              <label htmlFor="passwd">Hasło</label><br></br>
-              <input type="password" id='passwd' name='Hasło'/><br></br><br></br>
+            <form id="login-form" className="login-form">
+              <label htmlFor="Login">Login</label>
+              <br></br>
+              <input type="text" id="login" name="Login" />
+              <br></br>
+              <label htmlFor="passwd">Hasło</label>
+              <br></br>
+              <input type="password" id="passwd" name="Hasło" />
+              <br></br>
+              <br></br>
               {/* <Button variant="primary" className='login-btn' onClick={handleClose}>Zaloguj</Button><br></br> */}
-              <input type="button" value="Zaloguj" className='btn btn-primary login-btn' onClick={handleClose} />
+              <input
+                type="button"
+                value="Zaloguj"
+                className="btn btn-primary login-btn"
+                onClick={handleClose}
+              />
             </form>
-          </center>          
+          </center>
         </Modal.Body>
         {/* <Modal.Footer>
         </Modal.Footer> */}
@@ -44,6 +51,5 @@ function LoginModal() {
     </>
   );
 }
-
 
 export default LoginModal;
