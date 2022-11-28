f47bea4eec9de90e83082d14b7cc390f01ace462
GTR1701 <dawidkowal04@gmail.com>
2022-11-28 16:59:44 +0100

    próba home

--- Client/src/components/formularz/formularz.jsx
+++ Client/src/components/formularz/formularz.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./formularz.css";
 
 function Formularz(...props) {
--- Client/src/components/ikonki/ikonki.jsx
+++ Client/src/components/ikonki/ikonki.jsx
@@ -1,40 +1,46 @@
 import React from "react";
-import IconsAPI from "../../services/icons-api";
 import "./ikonki.css";
 import { FaRegHandshake } from "react-icons/fa";
 import { FaBolt } from "react-icons/fa";
 import { FaHome } from "react-icons/fa";
 
-const icons = new IconsAPI();
-let ikonki = icons.getIcons();
-
 const IconShow = () => {
   return (
     <div className="slide-container">
-        {ikonki.map((iconfile) => (
-          <div className="eachicon" key={iconfile.id}>
-            <div>   
-            <hr id="linia"/>
-            <hr/>
-            <div className="App">
-            <table>
-        <tr id="ramka">
-          <th><FaRegHandshake/></th>
-          <th><FaHome/></th>
-          <th><FaBolt/></th>
-        </tr>
-        <tr>
-          <td className="opis1">Współpraca z klientem jest jednym z najważniejszych aspektów naszej firmy.</td>
-          <td className="opis2">W naszym biurze obsługi klienta pomożemy ci bezstresowo rozwiązać twój problem.</td>
-          <td className="opis3">W zakładce pomoc można błyskawicznie zgłosić swój problem.</td>
-        </tr>
-      </table>
-    </div>
-            </div>
+      <div className="eachicon">
+        <div>
+          <hr id="linia" />
+          <hr />
+          <div className="App">
+            <table className="miejsce">
+              <tr className="miejsce">
+                <th>
+                  <FaRegHandshake />
+                </th>
+                <th>
+                  <FaHome />
+                </th>
+                <th>
+                  <FaBolt />
+                </th>
+              </tr>
+              <tr>
+                <td className="opis1">
+                  Współpraca z klientem jest jednym z najważniejszych aspektów
+                  naszej firmy.
+                </td>
+                <td className="opis2">
+                  W naszym biurze obsługi klienta pomożemy ci bezstresowo
+                  rozwiązać twój problem.
+                </td>
+                <td className="opis3">
+                  W zakładce pomoc można błyskawicznie zgłosić swój problem.
+                </td>
+              </tr>
+            </table>
           </div>
-        
-        ))}
-     
+        </div>
+      </div>
     </div>
   );
 };
