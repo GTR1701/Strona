d6a62e2a5b01b8463d01479f53c78d39449d4587
GTR1701 <dawidkowal04@gmail.com>
2022-11-28 18:20:44 +0100

    Data Przyjęcia

--- Server/index.js
+++ Server/index.js
@@ -4,11 +4,14 @@ const PORT = 6969;
 const cors = require("cors");
 const testRouter = require('./API/dbQuery/router')
 const formRouter = require('./API/formrouter')
+const update = require('./API/dbQuery/testclass')
+const recieveRoute = require('./API/recieveRoute')
 
 app.use(cors());
 app.use(express.json());
 app.use('/test', testRouter)
 app.use('/form', formRouter)
+app.use('/update', recieveRoute)
 
 app.listen(PORT, () => {
   console.log("Server running");
